353dfde3e993e4ecb991a4ad29875a29407651ca
felipe <fm3209742@gmail.com>
2021-09-20 01:29:04 -0300

    Updates

--- server.js
+++ server.js
@@ -1,52 +1,55 @@
 "use-strict"
 
-const ft = require("fastify")({logger: true})
+import fastify from 'fastify'
+import {join} from 'path'
+import { Low, JSONFile } from 'lowdb'
+import lodash from 'lodash'
 
-ft.register(require("fastify-cookie"))
-ft.register(require("fastify-csrf"))
-ft.register(
-	require('fastify-leveldb'),
-	{ name: 'db' }
-)
+const jsonData = new JSONFile("./data.json")
+const db = new Low(jsonData)
+const ft = fastify()
 
-// ft.register(require("fastify-redis"), {host: "127.0.0.1"})
-ft.register(require("fastify-compress"), {global: true})
-ft.register(require("fastify-bcrypt"), {saltWorkFactor: 12})
+db.read()
+db.data = db.data || { users: [] }
 
-ft.route({
-	method: "POST",
-	url: "/login",
-	onRequest: ft.csrfProtection,
-	handler: async (request, reply) => {
+ft.register(await import("fastify-cookie"))
+ft.register(await import("fastify-csrf"))
 
-		const user = await ft.level.db.get(request.body.email),
-		finalUser = JSON.parse(user),
-		isUser = await ft.bcrypt.compare(request.body.password, finalUser.hash)
-
-		isUser && reply.compress(user)
-	}
-})
+ft.register(await import("fastify-compress"), {global: true})
+ft.register(await import("fastify-bcrypt"), {saltWorkFactor: 12})
 
 ft.route({
 	method: "POST",
 	url: "/signin",
 	onRequest: ft.csrfProtection,
 	handler: async (request, reply) => {
-		const {body: {name, email, password}} = request
-		const hash = await ft.bcrypt.hash(request.body.password)
-		await ft.level.db.set(email, JSON.striginfy({name, email, hash, type: "aluno"}))
+		const {body: {name, email, password}} = request,
+		hash = await ft.bcrypt.hash(password)
+		db.data.users.push({name, email, hash, type: "aluno"})
+        await db.write()
 		return reply.code(200)
 	}
 })
 
 ft.route({
 	method: "POST",
-	url: "/login/teacher",
+	url: "/login",
 	onRequest: ft.csrfProtection,
+	handler: async (request, reply) => { 
+        const finalUser = db.data.users.find( el => el.email === request.body.email  )
+        const isHashed = await ft.bcrypt.compare(request.body.password, finalUser.hash)
+          
+        isHashed && reply.code(200).compress(finalUser)
+	}
+})
+
+ft.route({
+	method: "POST",
+	url: "/login/teacher",
+	onRequest: ft.csrfProtection, 
 	handler: (request, reply) => {
 		return reply.send("teacher")
 	}
 })
-
 
 ft.listen(3000, "0.0.0.0")
